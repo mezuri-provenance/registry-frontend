e489cd339e933b042338f6bc6e7490f491187463
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-05-11 22:04:40 -0700

    Fetch all sources from registry

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import React, {PureComponent} from 'react';
+import React, {Component} from 'react';
+
+import logo from './logo.svg';
 import './App.css';
-import DataComponent, {ComponentInfo} from './DataComponent';
 
+const registryUrl = 'http://127.0.0.1:5000/sources';
 
-const timeSeries: ComponentInfo = {
-  name: 'time-series',
-  description: 'Represents a sequence of points over successive time',
-  version: '0.0.2'
-};
+class App extends Component {
+  state = {
+    sources: null,
+  };
 
-class App extends PureComponent {
+  componentDidMount() {
+    fetch(registryUrl)
+        .then(res => res.json())
+        .then(data => this.setState({sources: data.components}))
+  }
+
   render() {
+    const {sources} = this.state;
+
     return (
       <div className="App">
-        <DataComponent component={timeSeries} />
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <div>
+          {sources
+              ? (sources.map(s => (
+                  <ComponentListItem key={s.name}>
+                    {s.name}
+                  </ComponentListItem>
+              )))
+              : <div>Loading...</div>
+          }
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+const ComponentListItem = ({children}) => (
+    <div style={{textAlign: 'left'}}>
+      {children}
+    </div>
+);
