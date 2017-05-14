3cf1dc2d71e42e43eef81a36ef7ddf73bf92bbee
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-05-14 13:56:58 -0700

    Remove obsolete code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -13,40 +12,6 @@ const getComponentVersionUrlFragmentByComponentType = componentType =>
     (name, version) => `${getComponentVersionsUrlFragmentByComponentType(componentType)(name)}/${version}`;
 
 const fetchJson = url => fetch(url).then(res => res.json());
-
-
-class ComponentDisplay extends Component {
-  state = {
-    component: null
-  };
-
-  componentDidMount() {
-    const {match} = this.props;
-
-    fetch(getComponentUrlFragmentByComponentType()())
-        .then(res => res.json())
-        .then(data => this.setState({component: data.sourceVersion}));
-  }
-
-  render() {
-    const {component} = this.state;
-
-    return (<div>
-      {component
-          ? <div>
-            Name: {component.name}
-          </div>
-          : <div>Loading...</div>}
-    </div>);
-  }
-}
-
-
-const ComponentListItem = ({children}) => (
-    <div style={{textAlign: 'left'}}>
-      {children}
-    </div>
-);
 
 
 class MezuriRegistryLoader extends Component {
@@ -161,7 +126,7 @@ function App() {
                         >
                           <MezuriSourceVersion />
                         </MezuriComponentHarness>
-                    )
+                    );
                   default:
                     return (
                         <div>
@@ -177,52 +142,5 @@ function App() {
   )
 }
 
-
-class AppOld extends Component {
-  state = {
-    sources: null,
-  };
-
-  componentDidMount() {
-    fetch(getComponentUrlFragmentByComponentType())
-        .then(res => res.json())
-        .then(data => {
-          const sources = {};
-          data.components.forEach(component => sources[component.name] = component);
-          this.setState({sources});
-        })
-  }
-
-  render() {
-    const {sources} = this.state;
-
-    return (
-        <Router>
-          <div className="App">
-            <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <h2>Welcome to React</h2>
-            </div>
-            <div style={{float: 'left'}}>
-              {sources
-                  ? (Object.values(sources).map(s => (
-                      <ComponentListItem key={s.name}>
-                        <Link to={`/${s.name}`}>
-                          {s.name}
-                        </Link>
-                      </ComponentListItem>
-                  )))
-                  : <div>Loading...</div>
-              }
-            </div>
-            <div style={{float: 'left'}}>
-              <h3>Component</h3>
-              <Route path="/:componentName" component={ComponentDisplay} />
-            </div>
-          </div>
-        </Router>
-    );
-  }
-}
 
 export default App;
