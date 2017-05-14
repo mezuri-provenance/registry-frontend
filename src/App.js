03369e609f7c9bc86404e9bbc04317b95e118b6e
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-05-14 10:17:47 -0700

    Add react-router and component route

--- src/App.js
+++ src/App.js
@@ -1,46 +1,38 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
 
-const registryUrl = 'http://127.0.0.1:5000/sources';
+const sourcesUrl = 'http://127.0.0.1:5000/sources';
+const getSourceUrl = componentName => `http://127.0.0.1:5000/sources/${componentName}`;
 
-class App extends Component {
+
+class ComponentDisplay extends Component {
   state = {
-    sources: null,
+    component: null
   };
 
   componentDidMount() {
-    fetch(registryUrl)
+    const {match} = this.props;
+
+    fetch(getSourceUrl(match.params.componentName))
         .then(res => res.json())
-        .then(data => this.setState({sources: data.components}))
+        .then(data => this.setState({component: data.component}));
   }
 
   render() {
-    const {sources} = this.state;
+    const {component} = this.state;
 
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div>
-          {sources
-              ? (sources.map(s => (
-                  <ComponentListItem key={s.name}>
-                    {s.name}
-                  </ComponentListItem>
-              )))
-              : <div>Loading...</div>
-          }
-        </div>
-      </div>
-    );
+    return (<div>
+      {component
+          ? <div>
+            Name: {component.name}
+          </div>
+          : <div>Loading...</div>}
+    </div>);
   }
 }
-
-export default App;
 
 
 const ComponentListItem = ({children}) => (
@@ -48,3 +40,53 @@ const ComponentListItem = ({children}) => (
       {children}
     </div>
 );
+
+
+class App extends Component {
+  state = {
+    sources: null,
+  };
+
+  componentDidMount() {
+    fetch(sourcesUrl)
+        .then(res => res.json())
+        .then(data => {
+          const sources = {};
+          data.components.forEach(component => sources[component.name] = component);
+          this.setState({sources});
+        })
+  }
+
+  render() {
+    const {sources} = this.state;
+
+    return (
+        <Router>
+          <div className="App">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React</h2>
+            </div>
+            <div style={{float: 'left'}}>
+              {sources
+                  ? (Object.values(sources).map(s => (
+                      <ComponentListItem key={s.name}>
+                        <Link to={`/${s.name}`}>
+                          {s.name}
+                        </Link>
+                      </ComponentListItem>
+                  )))
+                  : <div>Loading...</div>
+              }
+            </div>
+            <div style={{float: 'left'}}>
+              <h3>Component</h3>
+              <Route path="/:componentName" component={ComponentDisplay} />
+            </div>
+          </div>
+        </Router>
+    );
+  }
+}
+
+export default App;
